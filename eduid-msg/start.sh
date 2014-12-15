eebc1c8c2e830b773f59b2c95dbf83ef6ead63a5
Fredrik Thulin <fredrik@thulin.net>
2014-12-15 14:46:22 +0100

    auto code reloading in development etc.

--- eduid-msg/start.sh
+++ eduid-msg/start.sh
@@ -4,5 +4,14 @@ set -e
 
 . /opt/eduid/bin/activate
 
+chgrp eduid /opt/eduid/etc/eduid_msg/eduid_msg.ini
+chmod 640 /opt/eduid/etc/eduid_msg/eduid_msg.ini
+
+celery_args="--loglevel INFO"
+if [ -f /opt/eduid/src/setup.py ]; then
+    celery_args="--loglevel DEBUG --autoreload"
+fi
+
 cd /opt/eduid/etc/eduid_msg
-exec celery worker --app=eduid_msg -E --loglevel INFO -u eduid $*
+
+exec celery worker --app=eduid_msg --events --uid eduid --gid eduid $celery_args $*
--- eduid-signup/start.sh
+++ eduid-signup/start.sh
@@ -11,9 +11,15 @@ chmod 640 /opt/eduid/etc/eduid-signup.ini
 
 ls -l /opt/eduid/etc/eduid-signup.ini
 
+pserve_args=""
+if [ -f /opt/eduid/src/setup.py ]; then
+    pserve_args="--reload"
+fi
+
 start-stop-daemon --start -c eduid:eduid --exec \
      /opt/eduid/bin/pserve -- /opt/eduid/etc/eduid-signup.ini \
      --pid-file /opt/eduid/run/eduid-signup.pid \
-     --log-file /var/log/eduid/eduid-signup.log
+     --log-file /var/log/eduid/eduid-signup.log \
+    --user=eduid --group=eduid $pserve_args
 
 echo $0: Exiting
