170ef31981659be36dabfde379c8e55ec9cbba7b
Fredrik Thulin <fredrik@thulin.net>
2015-01-14 08:35:09 +0100

    move createUsers.sh from /data to /opt/eduid/db-scripts

--- mongodb/start.sh
+++ mongodb/start.sh
@@ -6,11 +6,11 @@ echo "$0: Starting mongod into background"
 
 sleep 2
 
-if [ -x /data/createUsers.sh ]; then
-    echo "$0: Creating eduid users"
-    /data/createUsers.sh
+if [ -x /opt/eduid/db-scripts/createUsers.sh ]; then
+    echo "$0: Creating eduid users using /opt/eduid/db-scripts"
+    /opt/eduid/db-scripts/createUsers.sh
 else
-    echo "$0: /data/createUsers.sh not executable"
+    echo "$0: /opt/eduid/db-scripts/createUsers.sh not executable"
 fi
 
 tail -F /var/log/mongodb/mongodb.log
