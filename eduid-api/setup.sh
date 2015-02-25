9b3090a4f9c8b639fb73e33f147866a933045f2e
Fredrik Thulin <fredrik@thulin.net>
2015-02-25 15:03:58 +0100

    use --pre version of eduid-api

--- eduid-api/setup.sh
+++ eduid-api/setup.sh
@@ -7,7 +7,7 @@ apt-get -y install \
     zlib1g-dev		# To get PIL zip compression
 
 #/opt/eduid/bin/pip install -i https://pypi.nordu.net/simple/ raven
-/opt/eduid/bin/pip install -i https://pypi.nordu.net/simple/ eduid-api
+/opt/eduid/bin/pip install --pre -i https://pypi.nordu.net/simple/ eduid-api
 /opt/eduid/bin/pip install -i https://pypi.nordu.net/simple/ jose
 /opt/eduid/bin/pip install -i https://pypi.nordu.net/simple/ vccs-client
 /opt/eduid/bin/pip install -i https://pypi.nordu.net/simple/ qrcode
@@ -15,3 +15,4 @@ apt-get -y install \
 /opt/eduid/bin/pip install -i https://pypi.nordu.net/simple/ requests
 /opt/eduid/bin/pip install --pre -i https://pypi.nordu.net/simple/ pyhsm
 
+/opt/eduid/bin/pip freeze
