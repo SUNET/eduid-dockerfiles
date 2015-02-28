a3a50df2bb0675cc5c4607821bff363f3e5b96f4
Fredrik Thulin <fredrik@thulin.net>
2015-02-28 18:50:24 +0100

    hmm, eduid-am didn't have beats I think

--- eduid-am/start.sh
+++ eduid-am/start.sh
@@ -13,10 +13,10 @@ cfg_dir=${cfg_dir-"${base_dir}/etc"}
 ini=${ini-"${cfg_dir}/${eduid_name}.ini"}
 log_dir=${log_dir-'/var/log/eduid'}
 logfile=${logfile-"${log_dir}/${eduid_name}.log"}
-state_dir=${state_dir-"${base_dir}/run"}
-celerybeat_file=${celerybeat_file-"${state_dir/celerybeat-schedule"}
+#state_dir=${state_dir-"${base_dir}/run"}
+#celerybeat_file=${celerybeat_file-"${state_dir/celerybeat-schedule"}
 
-chown eduid: "${log_dir}"
+chown eduid: "${log_dir}" #"${state_dir}"
 
 # || true to not fail on read-only cfg_dir
 chgrp eduid "${ini}" || true
@@ -36,5 +36,5 @@ cd "${cfg_dir}"
 
 exec celery worker --app=eduid_am --events --uid eduid --gid eduid \
     --logfile="${logfile}" \
-    -s "${celerybeat_file}" \
+    #-s "${celerybeat_file}" \
     $celery_args
