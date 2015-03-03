454892af8a3c83723f171ddf66591e4d513a2c13
Fredrik Thulin <fredrik@thulin.net>
2015-03-03 14:36:01 +0100

    make work like our other celery workers

--- eduid-lookup-mobile/setup.sh
+++ eduid-lookup-mobile/setup.sh
@@ -2,4 +2,6 @@
 
 set -e
 
-/opt/eduid/bin/pip install -i https://pypi.nordu.net/simple/ eduid_lookup_mobile
+/opt/eduid/bin/pip install --pre -i https://pypi.nordu.net/simple/ eduid_lookup_mobile
+
+/opt/eduid/bin/pip freeze
--- eduid-lookup-mobile/start.sh
+++ eduid-lookup-mobile/start.sh
@@ -1,15 +1,42 @@
 #!/bin/sh
 
 set -e
+set -x
 
 # activate python virtualenv
 . /opt/eduid/bin/activate
 
+# These could be set from Puppet if multiple instances are deployed
+eduid_name=${eduid_name-'eduid-lookup-mobile'}
+# this is a Python module name, so can't have hyphen (also name of .ini file)
+app_name=$(echo $eduid_name | tr "-" "_")
+base_dir=${base_dir-"/opt/eduid/${eduid_name}"}
+# These *can* be set from Puppet, but are less expected to...
+cfg_dir=${cfg_dir-"${base_dir}/etc"}
+ini=${ini-"${cfg_dir}/${app_name}.ini"}
+log_dir=${log_dir-'/var/log/eduid'}
+logfile=${logfile-"${log_dir}/${eduid_name}.log"}
+
+chown eduid: "${log_dir}"
+
+# || true to not fail on read-only cfg_dir
+chgrp eduid "${ini}" || true
+chmod 640 "${ini}" || true
+
 celery_args="--loglevel INFO"
 if [ -f /opt/eduid/src/setup.py ]; then
     celery_args="--loglevel DEBUG --autoreload"
+else
+    if [ -f "${cfg_dir}/${app_name}_DEBUG" ]; then
+	# eduid-dev environment
+	celery_args="--loglevel DEBUG"
+    fi
 fi
 
-cd /opt/eduid/src
+cd "${cfg_dir}"
 
-celery worker --app=eduid_lookup_mobile --events --uid eduid --gid eduid $celery_args $*
+echo "$0: Starting Celery app '${app_name}' in directory ${cfg_dir}"
+exec celery worker --app="${app_name}" --events --uid eduid --gid eduid \
+    --logfile="${logfile}" \
+    $celery_args
+
