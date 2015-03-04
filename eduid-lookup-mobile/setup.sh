ca83aeeb978bc4e617dda8b7ac19ccf4f4ab9063
Fredrik Thulin <fredrik@thulin.net>
2015-03-04 13:08:52 +0100

    install eduid_msg

--- eduid-lookup-mobile/setup.sh
+++ eduid-lookup-mobile/setup.sh
@@ -2,6 +2,17 @@
 
 set -e
 
+# APT dependencys for eduid_msg
+apt-get update
+apt-get -y install \
+    libxml2-dev \
+    libxslt-dev \
+    zlib1g-dev
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
 /opt/eduid/bin/pip install --pre -i https://pypi.nordu.net/simple/ eduid_lookup_mobile
+# until eduid_msg is a setup.py dependency of eduid_lookup_mobile
+/opt/eduid/bin/pip install --pre -i https://pypi.nordu.net/simple/ eduid_msg
 
 /opt/eduid/bin/pip freeze
