598b0a2e1330a0ee80299d1692112450510f3d14
Fredrik Thulin <fredrik@thulin.net>
2015-05-04 15:37:01 +0200

    remove unused state_dir

--- eduid-api/start.sh
+++ eduid-api/start.sh
@@ -12,11 +12,10 @@ base_dir=${base_dir-"/opt/eduid/${eduid_name}"}
 # These *can* be set from Puppet, but are less expected to...
 cfg_dir=${cfg_dir-"${base_dir}/etc"}
 log_dir=${log_dir-'/var/log/eduid'}
-state_dir=${state_dir-"${base_dir}/run"}
 ini=${ini-"${cfg_dir}/${eduid_name}.ini"}
 run=${run-'/opt/eduid/bin/eduid_api'}
 
-chown eduid: "${log_dir}" "${state_dir}"
+chown eduid: "${log_dir}"
 
 # || true to not fail on read-only cfg_dir
 chgrp eduid "${ini}" || true
@@ -27,6 +26,12 @@ if [ -x /opt/eduid/src/src/eduid_api/eduid_apibackend.py ]; then
     run=/opt/eduid/src/src/eduid_api/eduid_apibackend.py
 fi
 
+# nice to have in docker run output, to check what
+# version of something is actually running.
+/opt/eduid/bin/pip freeze
+
+echo ""
+echo "$0: Starting ${run} with config ${ini}"
 start-stop-daemon --start -c eduid:eduid --exec \
      /opt/eduid/bin/python -- $run \
     --config-file $ini \
