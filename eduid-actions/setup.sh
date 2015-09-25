b884436e02da571c5f2798760d0a24cd2d5991b4
Fredrik Thulin <fredrik@thulin.net>
2015-09-25 15:50:11 +0200

    init

---
+++ eduid-actions/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+#apt-get -y install \
+
+PYPI="https://pypi.nordu.net/simple/"
+ping -c 1 -q pypiserver.docker && PYPI="http://pypiserver.docker:8080/simple/"
+
+echo "#############################################################"
+echo "$0: Using PyPi URL ${PYPI}"
+echo "#############################################################"
+
+/opt/eduid/bin/pip install --pre -i ${PYPI} eduid-actions
+/opt/eduid/bin/pip install       -i ${PYPI} raven
+
+/opt/eduid/bin/pip freeze
