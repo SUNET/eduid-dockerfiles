ebf357b25b497505a2c9b60bc1fd1d87b17b0f42
Enrique Pérez Arnaud <enrique@cazalla.net>
2015-10-13 18:56:55 +0200

    (temporary until eduid_action.tou is in pypi.nordu.net) install dependencies for eduid_action.tou in the am container

--- eduid-am/setup.sh
+++ eduid-am/setup.sh
@@ -14,6 +14,13 @@ echo "#############################################################"
 #/opt/eduid/bin/pip install -i https://pypi.nordu.net/simple/ eduid-api-amp
 /opt/eduid/bin/pip install --pre -i ${PYPI} eduid-signup-amp
 /opt/eduid/bin/pip install --pre -i ${PYPI} eduid-dashboard-amp
-/opt/eduid/bin/pip install --pre -i ${PYPI} eduid_action.tou
+# /opt/eduid/bin/pip install --pre -i ${PYPI} eduid_action.tou
+# XXX the four packages below are dependencies for eduid_action.tou.
+# Once eduid_action.tou is in pypi.nordu.net, the line above can be
+# uncommented and the ones below deleted.
+/opt/eduid/bin/pip install --pre -i ${PYPI} eduid-userdb
+/opt/eduid/bin/pip install --pre -i ${PYPI} eduid-actions
+/opt/eduid/bin/pip install 'Babel==1.3'
+/opt/eduid/bin/pip install 'lingua==1.5'
 
 /opt/eduid/bin/pip freeze
