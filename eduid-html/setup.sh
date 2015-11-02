8077d2f84f9148021bb9e32f22482dd5d162b0bf
Fredrik Thulin <fredrik@thulin.net>
2015-11-02 12:19:31 +0100

    make more like the others
    main change is to host the wwwroot under /opt/eduid/eduid-html

--- eduid-html/setup.sh
+++ eduid-html/setup.sh
@@ -8,15 +8,13 @@ apt-get -y install \
 
 # make sure we get rid of all default sites
 rm -f /etc/nginx/sites-enabled/*
-# sym-link the config we want
-ln -s /opt/eduid/sites-available/html.conf /etc/nginx/sites-enabled/html.conf
 
 # Get the data from github
-mkdir -p /opt/eduid/src/
-cd /opt/eduid/src/
-git clone https://github.com/SUNET/eduid-html.git
+mkdir -p /opt/eduid/eduid-html/
+cd /opt/eduid/eduid-html/
+git clone https://github.com/SUNET/eduid-html.git wwwroot
 # tmp change branch
-cd eduid-html
+cd /opt/eduid/eduid-html/wwwroot
 git checkout tmp_new_design
 
 # save space
