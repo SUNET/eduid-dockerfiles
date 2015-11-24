2db0421286b987dc06327316f6bba59de198bc69
Fredrik Thulin <fredrik@thulin.net>
2015-11-24 15:58:01 +0100

    init

---
+++ nagios3/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+set -x
+
+apt-get update
+apt-get -y install \
+    fcgiwrap \
+    php5-fpm \
+    supervisor \
+    nagios3 \
+    libjson-perl \
+    liburi-perl \
+    libnagios-plugin-perl \
+    nagios-nrpe-plugin \
+    nagios-plugins-contrib \
+    nsca-client
+
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+
+PYPI="https://pypi.nordu.net/simple/"
+ping -c 1 -q pypiserver.docker && PYPI="http://pypiserver.docker:8080/simple/"
+
+echo "#############################################################"
+echo "$0: Using PyPi URL ${PYPI}"
+echo "#############################################################"
+
+/opt/eduid/bin/pip install -i ${PYPI} jose
+
+/opt/eduid/bin/pip freeze
---
+++ nagios3/start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. /opt/eduid/bin/activate
+
+# These could be set from Puppet if multiple instances are deployed
+eduid_name=${eduid_name-'nagios3'}
+base_dir=${base_dir-"/opt/eduid/${eduid_name}"}
+# These *can* be set from Puppet, but are less expected to...
+cfg_dir=${cfg_dir-"${base_dir}/etc"}
+log_dir=${log_dir-'/var/log/nagios3'}
+state_dir=${state_dir-"${base_dir}/run"}
+cache_dir=${cache_dir-'/var/cache/nagios3'}
+extra_args=${extra_args-''}
+
+rm -rf "${cache_dir}"
+mkdir -p "${cache_dir}"
+chmod 770 "${cache_dir}"
+
+for dir in "${log_dir}" "${state_dir}" "${cache_dir}"; do
+    test -d "${dir}" && chown -R nagios: "${dir}"
+done
+
+# nice to have in docker run output, to check what
+# version of something is actually running.
+/opt/eduid/bin/pip freeze
+
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
