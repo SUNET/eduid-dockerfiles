dbb38e99176e281c7bf1bd3dae3b62ca7f7e2589
John Van de Meulebrouck Brendgard <john@nordu.net>
2015-12-15 17:32:04 +0100

    Added build dependecies for Pillow

--- eduid-idproofing-letter/setup.sh
+++ eduid-idproofing-letter/setup.sh
@@ -3,6 +3,19 @@
 set -e
 set -x
 
+# The below are needed for Pillow
+apt-get update
+apt-get -y install \
+    libtiff5-dev \
+    libjpeg8-dev \
+    zlib1g-dev \
+    libfreetype6-dev \
+    liblcms2-dev \
+    libwebp-dev \
+    tcl8.6-dev \
+    tk8.6-dev \
+    python-tk
+
 PYPI="https://pypi.nordu.net/simple/"
 ping -c 1 -q pypiserver.docker && PYPI="http://pypiserver.docker:8080/simple/"
 
