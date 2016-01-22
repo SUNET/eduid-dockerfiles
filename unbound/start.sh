8481d50170025472c27ef8b5c4087804febf79dc
Fredrik Thulin <fredrik@thulin.net>
2016-01-22 13:58:29 +0100

    UNBOUND_INTERFACE config fixup

--- unbound/start.sh
+++ unbound/start.sh
@@ -18,4 +18,6 @@ echo "$0: Unbound config"
 cat "${cfg_dir}/unbound.conf"
 echo ---
 
+test -d /etc/unbound/unbound.conf.d/ || mkdir -p /etc/unbound/unbound.conf.d/
+
 exec /usr/sbin/unbound $verbose -dc "${cfg_dir}/unbound.conf"
