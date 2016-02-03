f467bb4b7d517e7ab6ee5987e09f8ffe28ef21f7
Johan Lundberg <lundberg@nordu.net>
2016-02-03 18:15:41 +0100

    Changed default workers to 1 and default timeout to 30s

--- eduid-idproofing-letter/start.sh
+++ eduid-idproofing-letter/start.sh
@@ -13,8 +13,8 @@ base_dir=${base_dir-"/opt/eduid/${eduid_name}"}
 cfg_dir=${cfg_dir-"${base_dir}/etc"}
 log_dir=${log_dir-'/var/log/eduid'}
 state_dir=${state_dir-"${base_dir}/run"}
-workers=${workers-4}
-worker_timeout=${worker_timeout-120}
+workers=${workers-1}
+worker_timeout=${worker_timeout-30}
 gunicorn_args="--bind 0.0.0.0:8080 -w ${workers} -t ${worker_timeout} ${app_name}:app"
 
 chown eduid: "${log_dir}" "${state_dir}"
@@ -26,5 +26,5 @@ chown eduid: "${log_dir}" "${state_dir}"
 echo "Reading settings from: ${IDPROOFING_LETTER_SETTINGS-'No settings file'}"
 exec start-stop-daemon --start -c eduid:eduid --exec \
      /opt/eduid/bin/gunicorn \
-     --pidfile "${state_dir}/${eduid-name}.pid" \
+     --pidfile "${state_dir}/${eduid_name}.pid" \
      --user=eduid --group=eduid -- $gunicorn_args
