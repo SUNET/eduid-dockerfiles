f111c0f2044cf4ba31f91122d19333dde9aba63c
Fredrik Thulin <fredrik@thulin.net>
2016-04-21 14:08:30 +0200

    fix module path for start command

--- eduid-oidc-proofing/start.sh
+++ eduid-oidc-proofing/start.sh
@@ -17,7 +17,7 @@ log_dir=${log_dir-'/var/log/eduid'}
 state_dir=${state_dir-"${base_dir}/run"}
 workers=${workers-1}
 worker_timeout=${worker_timeout-30}
-gunicorn_args="--bind 0.0.0.0:8080 -w ${workers} -t ${worker_timeout} run"
+gunicorn_args="--bind 0.0.0.0:8080 -w ${workers} -t ${worker_timeout} eduid_webapp.oidc_proofing.run:app"
 
 chown eduid: "${log_dir}" "${state_dir}"
 
