7d277c1a4af6d4c260f21fdf46ce2ffd494f00c1
Johan Lundberg <lundberg@nordu.net>
2016-04-22 14:52:04 +0200

    Added worker_threads to oidc_proofing start.sh

--- eduid-oidc-proofing/start.sh
+++ eduid-oidc-proofing/start.sh
@@ -17,8 +17,9 @@ log_dir=${log_dir-'/var/log/eduid'}
 state_dir=${state_dir-"${base_dir}/run"}
 workers=${workers-1}
 worker_class=${worker_class-sync}
+worker_threads=${worker_threads-1}
 worker_timeout=${worker_timeout-30}
-gunicorn_args="--bind 0.0.0.0:8080 -w ${workers} -k ${worker_class} -t ${worker_timeout} eduid_webapp.oidc_proofing.run:app"
+gunicorn_args="--bind 0.0.0.0:8080 -w ${workers} -k ${worker_class} --threads ${worker_threads} -t ${worker_timeout} eduid_webapp.oidc_proofing.run:app"
 
 chown eduid: "${log_dir}" "${state_dir}"
 
