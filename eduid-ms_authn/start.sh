bd4ef29bf797153037a6e4e2f478b7bc2702f397
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-05-12 15:01:32 +0200

    working on authentication service

--- eduid-ms_authn/start.sh
+++ eduid-ms_authn/start.sh
@@ -11,9 +11,13 @@ base_dir="${base_dir-/opt/eduid}"
 cfg_dir="${cfg_dir-${base_dir}/etc}"
 log_dir="${log_dir-/var/log/eduid}"
 state_dir="${state_dir-${base_dir}/run}"
-ini="${ini-${cfg_dir}/${eduid_name}.ini}"
+config="${ini-${cfg_dir}/${eduid_name}.ini}"
 saml2_settings="${saml2_settings-${cfg_dir}/saml2_settings.py}"
 run="${state_dir}/${eduid_name}.pid"
+workers="${workers-1}"
+worker_class="${worker_class-sync}"
+worker_threads="${worker_threads-1}"
+worker_timeout="${worker_timeout-30}"
 
 chown eduid: "${log_dir}" "${state_dir}"
 
@@ -21,23 +25,27 @@ chown eduid: "${log_dir}" "${state_dir}"
 chgrp eduid "${ini}" || true
 chmod 640 "${ini}" || true
 
+# nice to have in docker run output, to check what
+# version of something is actually running.
+/opt/eduid/bin/pip freeze
+
 extra_args=""
 if [ -f "/opt/eduid/src/eduid-webapp/setup.py" ]; then
     # developer mode, restart on code changes
     extra_args="--reload"
 fi
 
-# nice to have in docker run output, to check what
-# version of something is actually running.
-/opt/eduid/bin/pip freeze
-
 echo ""
 echo "$0: Starting ${run} with config ${ini}"
-gunicorn --pid ${run} \
-        --env EDUID_CONFIG=${ini} \
-        --env SAML2_SETTINGS_MODULE=${saml2_settings} \
-        --access-logfile ${log_dir}/access.log \
-        --error-logfile ${log_dir}/error.log \
-        --bind 0.0.0.0:8080 \
-        ${extra_args} \
-        eduid_webapp.authn.run:app
+
+exec start-stop-daemon --start -c eduid:eduid --exec \
+    /opt/eduid/bin/gunicorn \
+    --bind 0.0.0.0:8080 \
+    --workers ${workers} --worker_class ${worker_class} \
+    --threads ${worker_threads} --timeout ${worker_timeout} \
+    --pid ${run} --user eduid --group eduid \
+    --env EDUID_INI_FILE_NAME=${config} \
+    --env SAML2_SETTINGS_MODULE=${saml2_settings} \
+    --access-logfile "${log_dir}/${eduid_name}-access.log" \
+    --error-logfile "${log_dir}/${eduid_name}-error.log" \
+    ${extra_args} eduid_webapp.authn.run:app
