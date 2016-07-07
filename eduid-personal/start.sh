c6925773d87991895d22fa9cdaefd0c7bc32b52f
Enrique Pérez Arnaud <enrique@cazalla.net>
2016-07-07 14:55:11 +0200

    personal data dockerfile

---
+++ eduid-personal/start.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+set -e
+set -x
+
+. /opt/eduid/bin/activate
+
+# These could be set from Puppet if multiple instances are deployed
+eduid_name="${eduid_name-eduid-personal-data}"
+base_dir="${base_dir-/opt/eduid}"
+project_dir="${project_dir-${base_dir}/eduid-webapp/src}"
+cfg_dir="${cfg_dir-${base_dir}/etc}"
+log_dir="${log_dir-${base_dir}/log}"
+state_dir="${state_dir-${base_dir}/run}"
+run="${state_dir}/${eduid_name}.pid"
+workers="${workers-1}"
+worker_class="${worker_class-sync}"
+worker_threads="${worker_threads-1}"
+worker_timeout="${worker_timeout-30}"
+
+chown eduid: "${log_dir}" "${state_dir}"
+
+# set PYTHONPATH if it is not already set using Docker environment
+export PYTHONPATH=${PYTHONPATH-${project_dir}}
+
+echo "PYTHONPATH=${PYTHONPATH}"
+
+# nice to have in docker run output, to check what
+# version of something is actually running.
+/opt/eduid/bin/pip freeze
+
+extra_args=""
+if [ -f "/opt/eduid/src/eduid-webapp/setup.py" ]; then
+    # developer mode, restart on code changes
+    extra_args="--reload"
+fi
+
+echo ""
+echo "$0: Starting ${run}"
+
+exec start-stop-daemon --start -c eduid:eduid --exec \
+    /opt/eduid/bin/gunicorn \
+    --pidfile ${run} --user eduid --group eduid -- \
+    --workers ${workers} --worker-class ${worker_class} \
+    --threads ${worker_threads} --timeout ${worker_timeout} \
+    --access-logfile "${log_dir}/${eduid_name}-access.log" \
+    --error-logfile "${log_dir}/${eduid_name}-error.log" \
+    --bind 0.0.0.0:8080 \
+    ${extra_args} eduid_webapp.personal_data.run:app
