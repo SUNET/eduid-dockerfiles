14ffadd1763c88bbaf8bbe07b3a7faf58919f61f
Johan Lundberg <lundberg@nordu.net>
2016-09-30 16:47:04 +0200

    Use same start.sh for microservices

--- eduid-letter-proofing/start.sh
+++ eduid-letter-proofing/start.sh
@@ -19,12 +19,17 @@ workers=${workers-1}
 worker_class=${worker_class-sync}
 worker_threads=${worker_threads-1}
 worker_timeout=${worker_timeout-30}
-gunicorn_args="--bind 0.0.0.0:8080 -w ${workers} -k ${worker_class} --threads ${worker_threads} -t ${worker_timeout} eduid_webapp.letter_proofing.run:app"
 
 chown eduid: "${log_dir}" "${state_dir}"
 
 # set PYTHONPATH if it is not already set using Docker environment
 export PYTHONPATH=${PYTHONPATH-${project_dir}}
+
+extra_args=""
+if [ -f "/opt/eduid/src/eduid-webapp/setup.py" ]; then
+    # developer mode, restart on code changes
+    extra_args="--reload"
+fi
 
 # nice to have in docker run output, to check what
 # version of something is actually running.
@@ -36,4 +41,10 @@ echo "Reading settings from: ${EDUID_CONFIG_NS-'No namespace set'}"
 exec start-stop-daemon --start -c eduid:eduid --exec \
      /opt/eduid/bin/gunicorn \
      --pidfile "${state_dir}/${eduid_name}.pid" \
-     --user=eduid --group=eduid -- $gunicorn_args
+     --user=eduid --group=eduid -- \
+     --bind 0.0.0.0:8080 \
+     --workers ${workers} --worker-class ${worker_class} \
+     --threads ${worker_threads} --timeout ${worker_timeout} \
+     --access-logfile "${log_dir}/${eduid_name}-access.log" \
+     --error-logfile "${log_dir}/${eduid_name}-error.log" \
+     ${extra_args} eduid_webapp.letter_proofing.run:app
