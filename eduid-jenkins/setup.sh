042dbd22a41ad9a7cb5a72b54c62e1d106fec4dc
Johan Lundberg <lundberg@nordu.net>
2016-10-25 13:21:53 +0200

    Added boilerplate packages and env vars

--- eduid-jenkins/setup.sh
+++ eduid-jenkins/setup.sh
@@ -5,8 +5,14 @@ set -e
 # Set exec bit on build script
 chmod 755 /opt/eduid/build.sh
 
-# Pre-install common or slow Python packages
-/opt/eduid/bin/pip install -U setuptools
-/opt/eduid/bin/pip install nose nosexcover pylint
+export VIRTUAL_ENV="/opt/eduid/"
+export PIP_DOWNLOAD_CACHE=/var/cache/jenkins/pip
+export PIP_INDEX_URL=https://pypi.nordu.net/simple/
 
+. $VIRTUAL_ENV/bin/activate
+
+# Pre-install common or slow Python packages
+pip install -U setuptools
+pip install nose nosexcover pylint mock==1.0.1
+pip install pysaml2==4.0.3rc1 "pymongo>=2.8,<3" redis>=2.10.5 PyNaCl>=1.0.1
 
