99c89139d6bc7967b593f723065f6665992e2533
Johan Lundberg <lundberg@nordu.net>
2016-12-06 18:27:17 +0100

    Install curl in eduid_msg container for Navet nagios check

--- eduid-msg/setup.sh
+++ eduid-msg/setup.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-# Not needed when not using pynavet, remove after deployment
-#apt-get update
-#apt-get -y install \
-#    libxml2-dev \
-#    libxslt-dev \
-#    zlib1g-dev
-#apt-get clean
-#rm -rf /var/lib/apt/lists/*
+# Install curl for nagios check
+apt-get update
+apt-get -y install \
+    curl
+apt-get clean
+rm -rf /var/lib/apt/lists/*
 
 PYPI="https://pypi.nordu.net/simple/"
 ping -c 1 -q pypiserver.docker && PYPI="http://pypiserver.docker:8080/simple/"
@@ -18,7 +16,6 @@ echo "#############################################################"
 echo "$0: Using PyPi URL ${PYPI}"
 echo "#############################################################"
 
-#/opt/eduid/bin/pip install --pre -i ${PYPI} suds
 /opt/eduid/bin/pip install --pre -i ${PYPI} eduid-msg
 
 /opt/eduid/bin/pip freeze
