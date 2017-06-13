8afb211b96f971ad7b2d9d7e9ad42a548b75ddf9
Fredrik Thulin <fredrik@thulin.net>
2017-06-13 10:30:09 +0200

    add etcd stuff

--- eduid-admintools/setup.sh
+++ eduid-admintools/setup.sh
@@ -10,5 +10,8 @@ echo "$0: Using PyPi URL ${PYPI}"
 echo "#############################################################"
 
 /opt/eduid/bin/pip install --pre -i ${PYPI} 'eduid-userdb'
+/opt/eduid/bin/pip install --pre -i ${PYPI} 'proquint==0.1.0'
+/opt/eduid/bin/pip install --pre -i ${PYPI} 'python-etcd==0.4.3'
+/opt/eduid/bin/pip install --pre -i ${PYPI} 'PyYAML==3.12'
 
 /opt/eduid/bin/pip freeze
