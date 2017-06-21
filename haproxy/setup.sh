b49c4db35a68ee2020855fbe86a9d2f0167af4b2
John Van de Meulebrouck Brendgard <john@sunet.se>
2017-06-21 13:29:32 +0200

    Stretch is the new stable so no need for backported haproxy

--- haproxy/setup.sh
+++ haproxy/setup.sh
@@ -2,15 +2,9 @@
 
 set -e
 
-# To replace the dependency on a wget:ed a tar.gz over http as seen in the
-# official haproxy image we get the equivalent version from Debian, which
-# Unfortunately is only available from backports.
-echo "deb http://ftp.debian.org/debian jessie-backports main" >> /etc/apt/sources.list
-
 apt-get update
 apt-get -y dist-upgrade
-apt-get -t jessie-backports -y install \
-    haproxy
+apt-get -y install haproxy
 
 # save space
 rm -rf /var/lib/apt/lists/*
