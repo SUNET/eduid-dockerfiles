d6a391326d800432a6cdd0bea677d796d7830193
Fredrik Thulin <fredrik@thulin.net>
2017-12-04 17:00:22 +0100

    fixes

--- haproxy/cfgcheck.sh
+++ haproxy/cfgcheck.sh
@@ -3,4 +3,4 @@
 set -e
 
 logger -i -t haproxy-cfgcheck "Checking config: $*"
-/usr/sbin/haproxy -c $* | logger -i -t haproxy-cfgcheck
+exec /usr/sbin/haproxy -c $* | logger -i -t haproxy-cfgcheck
--- haproxy/reload.sh
+++ haproxy/reload.sh
@@ -6,6 +6,6 @@ logger -i -t haproxy-reload "Checking config: $*"
 
 /usr/sbin/haproxy -c $*
 
-logger -i -t haproxy-reload "Config checked OK, reloading"
+logger -i -t haproxy-reload "Config checked OK, reloading PID `cat /run/haproxy.pid`"
 
-exec haproxy-systemd-wrapper -p <%= @haproxy_pidfile %> -st $(cat <%= @haproxy_pidfile %>)
+exec haproxy-systemd-wrapper -p /run/haproxy.pid -st /run/haproxy.pid
