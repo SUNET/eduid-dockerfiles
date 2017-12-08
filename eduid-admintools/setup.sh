037a0285768b45bf3c8aa2dba79914e4f5b3b43d
Fredrik Thulin <fredrik@thulin.net>
2017-12-08 16:03:01 +0100

    try python-etcd 0.4.5 instead of the sed fix

--- eduid-admintools/setup.sh
+++ eduid-admintools/setup.sh
@@ -11,10 +11,10 @@ echo "#############################################################"
 
 /opt/eduid/bin/pip install --pre -i ${PYPI} 'eduid-userdb'
 /opt/eduid/bin/pip install --pre -i ${PYPI} 'proquint==0.1.0'
-/opt/eduid/bin/pip install --pre -i ${PYPI} 'python-etcd==0.4.3'
+/opt/eduid/bin/pip install --pre -i ${PYPI} 'python-etcd==0.4.5'
 /opt/eduid/bin/pip install --pre -i ${PYPI} 'PyYAML==3.12'
 
 # Need to upgrade TLSv1 to TLSv1_2 to be able to connect to our etcd v3 nodes
-sed -i -e 's/ssl.PROTOCOL_TLSv1$/ssl.PROTOCOL_TLSv1_2/' /opt/eduid/local/lib/python2.7/site-packages/etcd/client.py
+#sed -i -e 's/ssl.PROTOCOL_TLSv1$/ssl.PROTOCOL_TLSv1_2/' /opt/eduid/local/lib/python2.7/site-packages/etcd/client.py
 
 /opt/eduid/bin/pip freeze
