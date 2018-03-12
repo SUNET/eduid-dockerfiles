1b5b25da6571deb3843a18c863c73c709d65c8a6
Fredrik Thulin <fredrik@thulin.net>
2018-03-12 09:52:18 +0100

    log to stdout, and try setting net.ipv{4,6}.ip_nonlocal_bind=1

--- haproxy/autoreload.sh
+++ haproxy/autoreload.sh
@@ -4,35 +4,40 @@ set -e
 
 HAPROXYCFG=${HAPROXYCFG-'/etc/haproxy/haproxy.cfg'}
 
+# Enable haproxy to bind to addresses even if they haven't been added to
+# the namespace yet
+sysctl net.ipv4.ip_nonlocal_bind=1
+sysctl net.ipv6.ip_nonlocal_bind=1
+
 for i in $(seq 10); do
     test -f "${HAPROXYCFG}" && break
     sleep 1
 done
 
 if [ ! -f "${HAPROXYCFG}" ]; then
-    logger -i -t haproxy-autoreload "haproxy config not found after 10 seconds: ${HAPROXYCFG}"
+    echo "$0: haproxy config not found after 10 seconds: ${HAPROXYCFG}"
     exit 1
 fi
 
 
-logger -i -t haproxy-autoreload "Checking config: ${HAPROXYCFG}"
+echo "$0: Checking config: ${HAPROXYCFG}"
 
 /usr/sbin/haproxy -c -f "${HAPROXYCFG}"
 
-logger -i -t haproxy-autoreload "Config ${HAPROXYCFG} checked OK, starting haproxy-systemd-wrapper"
+echo "$0: Config ${HAPROXYCFG} checked OK, starting haproxy-systemd-wrapper"
 /usr/sbin/haproxy-systemd-wrapper -p /run/haproxy.pid -f "${HAPROXYCFG}" &
 
 while [ 1 ]; do
     # Block until an inotify event says that the config file was replaced
     inotifywait -e moved_to "${HAPROXYCFG}"
 
-    logger -i -t haproxy-autoreload "Move-to event triggered, checking config: ${HAPROXYCFG}"
+    echo "$0: Move-to event triggered, checking config: ${HAPROXYCFG}"
     config_ok=1
     /usr/sbin/haproxy -c -f "${HAPROXYCFG}" || config_ok=0
     if [ $config_ok = 1 ]; then
-	logger -i -t haproxy-autoreload "Config ${HAPROXYCFG} checked OK, gracefully restarting haproxy-systemd-wrapper"
+	echo "$0: Config ${HAPROXYCFG} checked OK, gracefully restarting haproxy-systemd-wrapper"
 	haproxy-systemd-wrapper $* -p /run/haproxy.pid -f "${HAPROXYCFG}" -sf `cat /run/haproxy.pid`
     else
-	logger -i -t haproxy-autoreload "Config ${HAPROXYCFG} NOT OK"
+	echo "$0: Config ${HAPROXYCFG} NOT OK"
     fi
 done
