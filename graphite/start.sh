eb7239a203956621f148eb1cac58a87db4525aba
Fredrik Thulin <fredrik@thulin.net>
2019-06-11 10:44:45 +0200

    fix perms of whisper db

--- graphite/start.sh
+++ graphite/start.sh
@@ -5,6 +5,6 @@ set -x
 
 mkdir -p /var/log/supervisor
 touch /supervisord.{log,pid}
-chown -R _graphite: /var/lib/graphite-api /var/log/graphite-api
+chown -R _graphite: /var/lib/graphite-api /var/log/graphite-api /var/lib/graphite/whisper
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
