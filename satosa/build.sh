844c37561dbba59fd8aa657f3cf3741a44386e9a
Fredrik Thulin <fredrik@thulin.net>
2020-03-16 16:36:26 +0100

    git clone, not docker clone

--- satosa/build.sh
+++ satosa/build.sh
@@ -3,6 +3,6 @@
 set -ex
 
 rm -rf satosa.build
-docker clone https://github.com/IdentityPython/SATOSA satosa.build
+git clone https://github.com/IdentityPython/SATOSA satosa.build
 docker build --tag "satosa-docker:latest"  --no-cache=true satosa.build
 docker tag "satosa-docker:latest" "docker.sunet.se/eduid/satosa:latest"
