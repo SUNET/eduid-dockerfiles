b1685950bc993955b6f0924645087564a0b2c2fc
Johan Lundberg <lundberg@sunet.se>
2020-11-11 16:07:40 +0100

    CI needs to learn about docker build args

--- telegraf/build.sh
+++ telegraf/build.sh
@@ -2,11 +2,9 @@
 
 set -ex
 
-# Remember to set TELEGRAF_VERSION and TELEGRAF_TAG för telegraf job in .jenkings.yaml if running on ci.sunet.se
-_jenkins_job=${JENKINS_JOB}
 _telegraf_version=${TELEGRAF_VERSION-$1}
 _telegraf_tag=${TELEGRAF_TAG-$2}
-_telegraf_image='local/telegraf'
+_telegraf_image=${TELEGRAF_IMAGE-'local/telegraf'}
 _release_build=false
 
 if [ -z "${_telegraf_version}" ]; then
@@ -29,7 +27,6 @@ fi
 
 docker pull debian:testing
 docker pull gcr.io/distroless/base
-if [ -z "${_jenkins_job}" ]; then
-  docker build --build-arg TELEGRAF_VERSION="${_telegraf_version}" --tag "${_telegraf_image}:latest"  --no-cache=true .
-  docker tag "${_telegraf_image}:latest" "${_telegraf_image}:${_telegraf_tag}"
-fi
+docker build --build-arg TELEGRAF_VERSION="${_telegraf_version}" --tag "${_telegraf_image}:latest"  --no-cache=true .
+docker tag "${_telegraf_image}:latest" "${_telegraf_image}:${_telegraf_tag}"
+docker push ${_telegraf_image}
