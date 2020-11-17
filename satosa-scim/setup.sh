2aa3f20475ee1bff30ae50cd25f944d59060923c
Johan Lundberg <lundberg@sunet.se>
2020-11-17 14:50:10 +0100

    build-essential and python3.7-dev needed for build

---
+++ satosa-scim/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -x
+
+export DEBIAN_FRONTEND noninteractive
+
+/bin/sed -i s/deb.debian.org/ftp.se.debian.org/g /etc/apt/sources.list
+
+apt-get update && \
+    apt-get -y dist-upgrade && \
+    apt-get install -y \
+      build-essential \
+      python3.7-dev \
+    && apt-get clean
+
+rm -rf /var/lib/apt/lists/*
+
