54b263e6f7e1983dd428e74881bf39bc6dd850cf
Johan Lundberg <lundberg@sunet.se>
2020-11-17 14:28:11 +0100

    Use env var REPLSET to start mongodb as a replicaSet of one

--- mongodb/start.sh
+++ mongodb/start.sh
@@ -28,10 +28,13 @@ else
     echo "$0: /opt/eduid/db-scripts/db_setup.py not executable or /opt/eduid/db-scripts/local.yaml does not exist"
 fi
 
+if [ "x$REPLSET" != "x" ]; then
+    mongo_args="--config ${dbconfig} --replSet rs0 --bind_ip 0.0.0.0 --dbpath /data --logpath /var/log/mongodb/mongodb.log"
+else
+    mongo_args="--config ${dbconfig} --bind_ip 0.0.0.0 --dbpath /data --logpath /var/log/mongodb/mongodb.log"
+fi
+
 echo "$0: Starting mongod into background"
 exec /sbin/start-stop-daemon --start -c mongodb:mongodb \
     --make-pidfile --pidfile $pidfile \
-    --exec /usr/bin/mongod -- \
-    --config $dbconfig \
-    --bind_ip 0.0.0.0 \
-    --dbpath /data --logpath /var/log/mongodb/mongodb.log
+    --exec /usr/bin/mongod -- $mongo_args
