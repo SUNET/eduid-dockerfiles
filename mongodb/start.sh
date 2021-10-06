7555d9d3eb4435a84fec023ecb5f5cbb48db3c86
Johan Lundberg <lundberg@sunet.se>
2021-10-06 16:02:41 +0200

    add support for automatically initiate a replica set

--- mongodb/start.sh
+++ mongodb/start.sh
@@ -9,29 +9,30 @@ if [ -s /opt/eduid/etc/mongod.conf ]; then
    dbconfig=/opt/eduid/etc/mongod.conf
 fi
 
+if [ "x$REPLSET" != "x" ]; then
+    mongo_args="--config ${dbconfig} --replSet rs0 --bind_ip 0.0.0.0 --dbpath /data --logpath /var/log/mongodb/mongodb.log"
+else
+    mongo_args="--config ${dbconfig} --bind_ip 0.0.0.0 --dbpath /data --logpath /var/log/mongodb/mongodb.log"
+fi
 
 if [ -s /opt/eduid/db-scripts/db_setup.py -a -s /opt/eduid/db-scripts/local.yaml ]; then
     echo "$0: Starting mongodb for db_setup.py"
     /sbin/start-stop-daemon --start -c mongodb:mongodb --background \
         --make-pidfile --pidfile $pidfile \
-        --exec /usr/bin/mongod -- \
-        --config $dbconfig \
-        --dbpath /data --logpath /var/log/mongodb/mongodb.log
+        --exec /usr/bin/mongod -- $mongo_args
     sleep 2
     echo "$0: Creating users and initializing databases using /opt/eduid/db-scripts/db_setup.py"
-    /opt/eduid/bin/python /opt/eduid/db-scripts/db_setup.py
+    if [ "x$REPLSET" != "x" ]; then
+        /opt/eduid/bin/python /opt/eduid/db-scripts/db_setup.py --replset rs0
+    else
+        /opt/eduid/bin/python /opt/eduid/db-scripts/db_setup.py
+    fi
     echo "$0: Stopping mongodb after running db_setup.py"
     /sbin/start-stop-daemon --stop -c mongodb:mongodb --pidfile $pidfile \
         --remove-pidfile
     sleep 2
 else
     echo "$0: /opt/eduid/db-scripts/db_setup.py not executable or /opt/eduid/db-scripts/local.yaml does not exist"
-fi
-
-if [ "x$REPLSET" != "x" ]; then
-    mongo_args="--config ${dbconfig} --replSet rs0 --bind_ip 0.0.0.0 --dbpath /data --logpath /var/log/mongodb/mongodb.log"
-else
-    mongo_args="--config ${dbconfig} --bind_ip 0.0.0.0 --dbpath /data --logpath /var/log/mongodb/mongodb.log"
 fi
 
 echo "$0: Starting mongod into background"
