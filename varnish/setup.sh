066c0c1bf9581401262e046cb4988711002d7ca6
Fredrik Thulin <fredrik@thulin.net>
2022-04-01 10:41:01 +0200

    try to fix the CI with a bogus commit

--- varnish/setup.sh
+++ varnish/setup.sh
@@ -4,6 +4,7 @@ set -e
 
 apt-get update
 apt-get -y dist-upgrade
+
 # Some of these are installed because they are used by the SUNET frontends
 # to check for interface status, other containers etc.
 apt-get -y install \
