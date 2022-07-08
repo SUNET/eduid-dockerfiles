73f6d1e4ace63719e88a3ba004312c71c9cdb163
Johan Lundberg <lundberg@sunet.se>
2022-07-08 17:29:17 +0200

    mongodb 3.6 -> 4.0

--- mongodb/setup.sh
+++ mongodb/setup.sh
@@ -2,5 +2,7 @@
 
 set -e
 
+python3 -m venv /opt/eduid
+/opt/eduid/bin/pip install -U pip wheel
 # for the db-scripts
-/opt/eduid/bin/pip install -i https://pypi.sunet.se/simple/ "pymongo>=3.6.0,==3.6.*" pyyaml
+/opt/eduid/bin/pip install -i https://pypi.sunet.se/simple/ "pymongo>=3.6.0,<4" pyyaml
